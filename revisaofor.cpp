4d592eefc67de1f35df254112f74b09902ae2ef8
jessicanobre <150728052+jessicanobre@users.noreply.github.com>
2024-04-26 00:41:49 +0000

    exercicios

---
+++ revisaofor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+int a = 0;
+
+while (a < 10)
+{
+    cout << "olá" << endl;
+    a = a + 1;
+}
+  for (int i = 0; i < 10; i++)
+  {
+    cout << "Palavra" << endl;
+  }
+
+   
+  
+    return 0;
+}
